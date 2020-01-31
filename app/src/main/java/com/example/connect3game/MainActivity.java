19002027d73eb89960401a1b9f1623e1630ba932
shruti.pahuja <shruti.pahuja@accenture.com>
2020-01-31 15:46:48 +0530

    Implementing 6x6 with two players

--- app/src/main/java/com/example/connect3game/MainActivity.java
+++ app/src/main/java/com/example/connect3game/MainActivity.java
@@ -15,8 +15,13 @@ public class MainActivity extends AppCompatActivity {
     //1 -> red_tile
     //2 -> empty
 
-    int[] originalGame = {2, 2, 2, 2, 2, 2, 2, 2, 2};
-    int[][] winnerGames = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
+
+    int[] originalGame = {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2};
+    int[][] winnerGames = {{0, 1, 2, 3, 4, 5}, {6, 7, 8, 9, 10, 11}, {12, 13, 14, 15, 16, 17},
+                           {18, 19, 20, 21, 22, 23}, { 24, 25, 26, 27, 28, 29}, {30, 31, 32, 33, 34, 35},
+                           {0, 6, 12, 18, 24, 30} , {1, 7, 13, 19, 25, 31}, {2, 8, 14, 20, 26, 32},
+                           {3, 9, 15, 21, 27, 33}, { 4, 10, 16, 22, 28, 34}, {5, 11, 17, 23, 29, 35},
+                           {0, 7, 14, 21, 28, 35}, {5, 10, 15, 20, 25, 30}};
     boolean currentGame = true;
 
     @Override
@@ -50,6 +55,9 @@ public class MainActivity extends AppCompatActivity {
 
                 if ((originalGame[winnerPositions[0]] == originalGame[winnerPositions[1]])
                         && (originalGame[winnerPositions[1]] == originalGame[winnerPositions[2]])
+                        && (originalGame[winnerPositions[2]] == originalGame[winnerPositions[3]])
+                        && (originalGame[winnerPositions[3]] == originalGame[winnerPositions[4]])
+                        && (originalGame[winnerPositions[4]] == originalGame[winnerPositions[5]])
                         && (originalGame[winnerPositions[2]] != 2)) {
 
                     currentGame = false;
@@ -91,7 +99,7 @@ public class MainActivity extends AppCompatActivity {
         //1 -> red_tile
         //2 -> empty
 
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < 35; i++) {
             originalGame[i] = 2;
         }
 
